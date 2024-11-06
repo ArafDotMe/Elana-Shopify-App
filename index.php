439b069cae71b55bab67e12d433d2dd52cd39d39
MAK <mak2022official@gmail.com>
2024-11-07 01:13:46 +0600

    update install index token

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php 
 include_once("includes/mysql_connect.php");
 
-// header("Location: install.php?shop=" . $_GET['shop']);
-// exit();
+$parameters = $_GET;
 
-echo "Araf";
+$query = "SELECT * FROM shops WHERE shop_url='". $parameters['shop'] ."' LIMIT 1";
+$result = $mysql->query($query);
+
+if( $result->num_rows < 1 ){
+    header("Location: install.php?shop=" . $_GET['shop']);
+    exit();
+}
+
+$store_data = $result->fetch_assoc();
+
+echo print_r($store_data);
+
+echo "Araf will be wealthy in very short time.";
--- token.php
+++ token.php
@@ -1,4 +1,5 @@
 <?php
+include_once("includes/mysql_connect.php");
 
 $api_key = '7e4980e11f58827f0ceaf0ca7e38dfa3';
 $secret_key = 'ab7b9fae77cbf98b988b18542beea0ed';
@@ -29,6 +30,13 @@ if( hash_equals($hmac, $new_hmac) ) {
     $response = json_decode($response, true);
 
     echo print_r($response);
+
+    $query = "INSERT INTO shops (shop_url, access_token, install_date) VALUES ('" . $shop_url . "','" . $response['access_token'] . "', NOW()) ON DUPLICATE KEY UPDATE access_token='" . $response['access_token'] . "'";
+    if($mysql->query($query)) {
+        echo "<script>top.window.location = 'https://" . $shop_url . "/admin/apps'</script>";
+        die;
+    }
+    
 }else {
     echo "Not OK";
 }
